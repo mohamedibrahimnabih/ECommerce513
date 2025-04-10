38048ba92ce57d4154de850059437700634f63b1
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-04-10 22:24:03 +0200

    Configure DB

---
+++ ECommerce513/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce513.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce513.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ECommerce513;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+    }
+}
