df2e80cd6cc324987192853da3128d4d604286d5
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-04-10 21:32:51 +0200

    First Controller & Action

---
+++ ECommerce513/Controllers/WelcomeController.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce513.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce513.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string name = "Mohamed";
+            int age = 25;
+            string address = "Mansoura";
+
+            //Person person = new()
+            //{
+            //    Name = name,
+            //    Age = age,
+            //    Address = address
+            //};
+
+            List<string> skills = [];
+            skills.Add("C++");
+            skills.Add("C#");
+            skills.Add("SQL Server");
+
+            var person = new
+            {
+                Name = name,
+                Age = age,
+                Address = address,
+                Skills = skills
+            };
+
+
+            return View(person);
+        }
+    }
+}
