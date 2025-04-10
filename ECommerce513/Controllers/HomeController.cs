c57dd18361de1248b5b8623a98e5388496d87592
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-04-10 22:46:40 +0200

    Home Page

--- ECommerce513/Controllers/HomeController.cs
+++ ECommerce513/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ECommerce513.Models;
+using ECommerce513.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce513.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context = new();
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -15,7 +18,9 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var products = _context.Products.Include(e => e.Category);
+
+        return View(products.ToList());
     }
 
     public IActionResult Privacy()
