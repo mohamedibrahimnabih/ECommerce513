6e639e5d9c815da6f5fd05a663bc906b00a6f1ed
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-04-17 21:02:20 +0200

    Read, Create on Category Model

---
+++ ECommerce513/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce513.Data;
+using ECommerce513.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce513.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories;
+
+            return View(categories.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
