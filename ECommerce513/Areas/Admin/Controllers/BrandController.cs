e3a71de440511abe260d8bb0db7e0f9ba7c0e348
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-04-17 22:29:02 +0200

    Update, Delete on Brand Model

--- ECommerce513/Areas/Admin/Controllers/BrandController.cs
+++ ECommerce513/Areas/Admin/Controllers/BrandController.cs
@@ -29,5 +29,41 @@ namespace ECommerce513.Areas.Admin.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Edit(int id)
+        {
+            var brand = _context.Brands.Find(id);
+
+            if (brand is not null)
+            {
+                return View(brand);
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Brand brand)
+        {
+            _context.Brands.Update(brand);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var brand = _context.Brands.Find(id);
+
+            if (brand is not null)
+            {
+                _context.Brands.Remove(brand);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction("NotFoundPage", "Home");
+        }
     }
 }
