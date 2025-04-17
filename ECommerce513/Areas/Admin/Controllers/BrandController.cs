ac8b254d2a8d2db8d36e0b4c92c4591b3a1ad44c
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-04-17 21:30:06 +0200

    Read, Create on Brand Model

---
+++ ECommerce513/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerce513.Data;
+using ECommerce513.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce513.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private readonly ApplicationDbContext _context = new();
+
+        public IActionResult Index()
+        {
+            var brands = _context.Brands;
+
+            return View(brands.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Brand brand)
+        {
+            _context.Brands.Add(brand);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
